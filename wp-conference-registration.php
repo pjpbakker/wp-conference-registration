a322a1a186bdca22c7086b23b786bef187e87922
Paul Bakker <bakker51@netscape.net>
2016-04-12 22:12:06 +0200

    first commit

---
+++ wp-conference-registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Conference Registration
+ * Plugin URI: https://github.com/pjpbakker/wp-conference-registration
+ * Description: Expsoses the Paypal API
+ * Version: 1.0
+ * Author: Paul Bakker
+ * Author URI: https://github.com/pjpbakker
+ * License: License: GPLv2 or later
+ */
